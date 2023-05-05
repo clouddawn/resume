ec96b706d7098b8a7a6c7c684dc52fe188445d33
clouddawn <1993456181@qq.com>
2023-05-05 16:23:32 +0800

    修复手机端echarts无法显示问题

--- dist/main.2bec9bd1.js
+++ dist/main.2bec9bd1.js
@@ -119,38 +119,38 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   return newRequire;
 })({"epB2":[function(require,module,exports) {
 // 基于准备好的dom，初始化echarts实例
-var myChart = echarts.init(document.getElementById('skills')); // 指定图表的配置项和数据
+var myChart = echarts.init(document.getElementById("skills")); // 指定图表的配置项和数据
 
 var option = {
   radar: {
     // shape: 'circle',
     indicator: [{
-      name: '静态页面',
+      name: "静态页面",
       max: 100
     }, {
-      name: '微信小程序',
+      name: "微信小程序",
       max: 100
     }, {
-      name: 'Vue 全家桶',
+      name: "Vue 全家桶",
       max: 100
     }, {
-      name: '项目开发能力',
+      name: "项目开发能力",
       max: 100
     }, {
-      name: '沟通能力',
+      name: "沟通能力",
       max: 100
     }]
   },
   series: [{
-    name: '能力雷达',
-    type: 'radar',
+    name: "能力雷达",
+    type: "radar",
     data: [{
       value: [100, 80, 90, 85, 80],
-      name: '能力指数'
+      name: "能力指数"
     }]
   }]
 }; // 使用刚指定的配置项和数据显示图表。
 
 myChart.setOption(option);
 },{}]},{},["epB2"], null)
-//# sourceMappingURL=main.fa6e3a39.js.map
+//# sourceMappingURL=main.2bec9bd1.js.map
